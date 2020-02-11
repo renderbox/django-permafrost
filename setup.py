6f2db8673076c13edba5f6c5f75bf2e748586ff1
Grant Viklund <grant.viklund@gmail.com>
2020-02-10 16:59:53 -0800

    Upped version to test Travis

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open(path.join(file_path, 'README.rst'), encoding='utf-8') as f:
 
 package_metadata = {
     'name': 'django-permafrost',
-    'version': '0.1.0',
+    'version': '0.1.1',
     'description': 'Adds Client Definable Permissions to Django',
     'long_description': long_description,
     'url': 'https://github.com/renderbox/django-permafrost/',
