30212eda69afb1e629f29e8a563574ac6f44ee9e
Grant Viklund <grant.viklund@gmail.com>
2020-02-11 17:50:24 -0800

    Added Django REST permission module.  It checks a user's permissions against what the View requires.

--- permafrost/views.py
+++ permafrost/views.py
@@ -84,3 +84,30 @@ class LogPermissionRequiredMixin(object):
                             )
 
         super().handle_no_permission()
+
+
+#--------------
+# DJANGO REST PERMS
+#--------------
+
+try:
+    from rest_framework.permission import BasePermission
+
+    class MethodPermissionRestMixin(BasePermission):
+
+        def has_permission(self, request, view):
+            perms = getattr(view, "permission_required", set() )
+            method_perms = getattr(view, "permission_required_" + self.request.method.lower(), set() )
+
+            check_list = list(perms) + list(method_perms)
+            user_perms = request.user.get_all_permissions()
+
+            # Get the user's perms and check that all the itms in the list are present
+            for perm in check_list:
+                if perm not in user_perms:
+                    return False
+
+            return True
+
+except ModuleNotFoundError:
+    pass
