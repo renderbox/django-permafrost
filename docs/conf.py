f2b37fc5a0f084415f40989a2492901391459afa
Grant Viklund <grant.viklund@gmail.com>
2020-06-05 14:28:25 -0700

    version update

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ copyright = '2020, Grant Viklund'
 author = 'Grant Viklund'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.29'
+release = '0.1.30'
 
 
 # -- General configuration ---------------------------------------------------
