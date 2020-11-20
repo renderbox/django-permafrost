f5fd318f80122f2f4f2e863c73437ead32f44632
Grant Viklund <grant.viklund@gmail.com>
2020-11-20 13:42:04 -0800

    Using the command line check instead.

--- develop/example/tests.py
+++ develop/example/tests.py
@@ -1,16 +1,16 @@
-from django.test import TestCase
-from django.core.management import call_command
+# from django.test import TestCase
+# from django.core.management import call_command
 
-class SystemTests(TestCase):
+# class SystemTests(TestCase):
 
-    def test_for_missing_migrations(self):
-        """ If no migrations are detected as needed, `result`
-        will be `None`. In all other cases, the call will fail,
-        alerting your team that someone is trying to make a
-        change that requires a migration and that migration is
-        absent.
-        Based on example by Scott Hacker
-        """
+#     def test_for_missing_migrations(self):
+#         """ If no migrations are detected as needed, `result`
+#         will be `None`. In all other cases, the call will fail,
+#         alerting your team that someone is trying to make a
+#         change that requires a migration and that migration is
+#         absent.
+#         Based on example by Scott Hacker
+#         """
 
-        result = call_command("makemigrations", check=True, dry_run=True)
-        self.assertIsNone(result)
+#         result = call_command("makemigrations", check=True, dry_run=True)
+#         self.assertIsNone(result)
