530afce54769f985d5edc8dfd53f13375a04ad3b
DeVonJ <devonj@whitemoondreams.com>
2020-11-24 15:26:14 +0000

    maps create url

--- permafrost/urls.py
+++ permafrost/urls.py
@@ -6,9 +6,9 @@ app_name = 'permafrost'
 
 urlpatterns = [
     path("roles/", views.PermafrostRoleListView.as_view(), name="role-list"),
+    path("role/add/", views.PermafrostRoleCreateView.as_view(), name="role-create"),
     path("role/<slug:slug>/", views.PermafrostRoleDetailView.as_view(), name="role-detail"),
     path("role/<slug:slug>/update/", views.PermafrostRoleUpdateView.as_view(), name="role-update"),
-    # path("role/add/", views.PermafrostRoleCreateView.as_view(), name="role-create"),
 ]
 
 
