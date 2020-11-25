0b4f5b98936dc43981983c4db68e26352f2bd30b
DeVonJ <devonj@whitemoondreams.com>
2020-11-25 17:53:56 +0000

    Adds selectpermafrostrole form as a 'interstitial' form

--- permafrost/forms.py
+++ permafrost/forms.py
@@ -1,9 +1,23 @@
 # Permafrost Forms
-from django.forms import ModelForm
+from django.forms import ModelForm, MultipleChoiceField, CheckboxSelectMultiple
+from django.forms.fields import CharField, ChoiceField
 from django.forms.widgets import Textarea
 from django.utils.translation import ugettext_lazy as _
+from .models import PermafrostRole, get_optional_by_category, get_required_by_category, get_choices
 
-from .models import PermafrostRole
+CHOICES = [('', _("Choose Role Type"))] + get_choices()
+
+
+class SelectPermafostRoleTypeForm(ModelForm):
+    name = CharField(required=False)
+    description = CharField(required=False, widget=Textarea())
+    category = ChoiceField(choices=CHOICES)
+    
+    class Meta:
+        model = PermafrostRole
+        fields = ('name', 'description', 'category',)
+
+
 class PermafrostRoleCreateForm(ModelForm):
     class Meta:
         model = PermafrostRole
