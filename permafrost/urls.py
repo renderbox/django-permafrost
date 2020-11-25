6d8885093ff56004a216ed15ae1b0ee0e568344f
DeVonJ <devonj@whitemoondreams.com>
2020-11-25 17:55:18 +0000

    Adds seperate create url for processing

--- permafrost/urls.py
+++ permafrost/urls.py
@@ -6,7 +6,8 @@ app_name = 'permafrost'
 
 urlpatterns = [
     path("roles/", views.PermafrostRoleListView.as_view(), name="role-list"),
-    path("role/add/", views.PermafrostRoleCreateView.as_view(), name="role-create"),
+    path("role/add/", views.select_role_type, name="role-select"),
+    path("role/create/", views.PermafrostRoleCreateView.as_view(), name="role-create"),
     path("role/<slug:slug>/", views.PermafrostRoleDetailView.as_view(), name="role-detail"),
     path("role/<slug:slug>/update/", views.PermafrostRoleUpdateView.as_view(), name="role-update"),
 ]
