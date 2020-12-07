93fecc1a177b7157c1d9d33a1ad7c483c57e3658
DeVonJ <devonj@whitemoondreams.com>
2020-12-07 18:28:52 +0000

    removes role add url

--- permafrost/urls.py
+++ permafrost/urls.py
@@ -6,9 +6,8 @@ app_name = 'permafrost'
 
 urlpatterns = [
     path("roles/", views.PermafrostRoleListView.as_view(), name="role-list"),
-    path("role/add/", views.select_role_type, name="role-select"),
     path("role/create/", views.PermafrostRoleCreateView.as_view(), name="role-create"),
-    # path("role/<slug:slug>/", views.PermafrostRoleDetailView.as_view(), name="role-detail"),
+    # path("role/<slug:slug>/", views.PermafrostRoleUpdateView.as_view(), name="role-detail"),
     path("role/<slug:slug>/", views.PermafrostRoleUpdateView.as_view(), name="role-update"),
 ]
 
