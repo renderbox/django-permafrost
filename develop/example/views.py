730ce55327c5fd752a8367a4d8a4820d67e7943d
Grant Viklund <grant.viklund@gmail.com>
2024-09-17 11:14:17 -0400

    flake8 pass

--- develop/example/views.py
+++ develop/example/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render
+# from django.shortcuts import render
 
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import authentication, permissions
-from django.contrib.auth.models import User
+
+# from rest_framework import authentication, permissions
+# from django.contrib.auth.models import User
 from django.views.generic import TemplateView
 
 from permafrost.permissions import PermafrostRESTPermission
